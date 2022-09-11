ed4a78e7f711964d67b895139d66a0af5c9a2746
ngkangting <benji.ngkt@gmail.com>
2022-09-11 10:54:05 +0800

    Added Bootstrap 5.2.1

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import { createPinia } from "pinia";
 import App from "./App.vue";
 import router from "./router";
 
+import "bootstrap/dist/css/bootstrap.min.css";
+
 import "./assets/main.css";
 
 const app = createApp(App);
@@ -12,3 +14,5 @@ app.use(createPinia());
 app.use(router);
 
 app.mount("#app");
+
+import "bootstrap/dist/js/bootstrap.js";
