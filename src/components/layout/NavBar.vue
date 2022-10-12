f5f1277c4f6082bcf6bb883461800a3b805c61dc
ngkangting <benji.ngkt@gmail.com>
2022-10-12 09:37:28 +0800

    Added Plan Now Page

--- src/components/layout/NavBar.vue
+++ src/components/layout/NavBar.vue
@@ -1,45 +1,89 @@
 <template>
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <nav class="navbar navbar-expand-lg bg-light">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#">Logo</a>
+      <router-link to="/" class="navbar-brand">ExploreLah!</router-link>
       <button
         class="navbar-toggler"
         type="button"
-        data-bs-toggle="collapse"
-        data-bs-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
+        data-bs-toggle="offcanvas"
+        data-bs-target="#offcanvasNavbar"
+        aria-controls="offcanvasNavbar"
       >
         <span class="navbar-toggler-icon"></span>
       </button>
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav me-auto mb-0 mb-lg-0">
-          <li class="nav-item">
-            <a class="nav-link" href="#">Discover</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">About Us</a>
-          </li>
-        </ul>
-        <ul
-          class="navbar-nav ms-auto mb-2 mb-lg-0"
-          v-if="!this.authStore.isLoggedIn"
-        >
-          <li class="nav-item">
-            <router-link to="/login" class="nav-link">Login</router-link>
-          </li>
-          <li class="nav-item">
-            <router-link to="/signup" class="nav-link">Sign Up</router-link>
-          </li>
-        </ul>
-        <ul class="navbar-nav ms-auto mb-2 mb-lg-0" v-else>
-          <li class="nav-item">
-            <router-link to="/" class="nav-link" @click="logoutUser()"
-              >Logout</router-link
-            >
-          </li>
-        </ul>
+      <div
+        class="offcanvas offcanvas-end w-100"
+        tabindex="-1"
+        id="offcanvasNavbar"
+        aria-labelledby="offcanvasNavbarLabel"
+      >
+        <div class="offcanvas-header">
+          <h5 class="offcanvas-title" id="offcanvasNavbarLabel">ExploreLah!</h5>
+          <button
+            type="button"
+            class="btn-close"
+            data-bs-dismiss="offcanvas"
+            aria-label="Close"
+          ></button>
+        </div>
+        <div class="offcanvas-body">
+          <ul class="navbar-nav flex-grow-1 pe-3">
+            <li class="nav-item">
+              <a class="nav-link" href="#">Discover</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">About Us</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">My Trips</a>
+            </li>
+          </ul>
+          <ul
+            class="navbar-nav justify-content-end flex-grow-1"
+            v-if="!this.authStore.isLoggedIn"
+          >
+            <li class="nav-item">
+              <router-link to="/login" class="nav-link">Login</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link to="/signup" class="nav-link">Sign Up</router-link>
+            </li>
+          </ul>
+          <!-- <ul class="navbar-nav justify-content-end flex-grow-1" v-else>
+            <li class="nav-item dropdown">
+              <a
+                class="nav-link bg-secondary rounded-circle d-flex justify-content-center align-items-center"
+                href="#"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+                style="width: 40px; height: 40px"
+              >
+                T
+              </a>
+              <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="#">Action</a></li>
+                <li><a class="dropdown-item" href="#">Another action</a></li>
+                <li>
+                  <hr class="dropdown-divider" />
+                </li>
+                <li>
+                  <router-link
+                    to="/"
+                    class="dropdown-item"
+                    @click="logoutUser()"
+                    >Logout</router-link
+                  >
+                </li>
+              </ul>
+            </li>
+            <li class="nav-item">
+              <router-link to="/" class="nav-link" @click="logoutUser()"
+                >Logout</router-link
+              >
+            </li>
+          </ul> -->
+        </div>
       </div>
     </div>
   </nav>
