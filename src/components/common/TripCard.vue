968cd31f540f756188951517d8110598143d1700
desireely <105425207+desireely@users.noreply.github.com>
2022-10-16 17:45:29 +0800

    My trips layout

---
+++ src/components/common/TripCard.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="card border-0 w-100">
+    <img src="../../assets/img/mbs.jpg" class="card-img-top rounded" alt="...">
+    <div class="card-body">
+      <p class="lead mb-1">Trip Name</p>
+      <p class="card-text">Description</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TripCard",
+  props: {
+    name
+  },
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style>
+
+</style>
--- src/views/MyTrips.vue
+++ src/views/MyTrips.vue
@@ -1,13 +1,60 @@
-<template>MyTrips idjiasdjasidjas</template>
+<template>
+  <div class="container-fluid ">
+    <div class="row mt-4 mb-5">
+      <h3 class="text-center mb-4 fw-bold">Relive your trips</h3>
+
+      <form class="d-flex w-50 mx-auto" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <i class="bi bi-search"></i>
+      </form>
+    </div>
+
+    <div class="row mx-4 mb-3">
+      <div class="col-lg-3 col-md-6">
+        <TripCard />
+      </div>
+      <div class="col-lg-3 col-md-6">
+        <TripCard />
+      </div>
+      <div class="col-lg-3 col-md-6">
+        <TripCard />
+      </div>
+      <div class="col-lg-3 col-md-6">
+        <TripCard />
+      </div>
+    </div>
+    <div class="row mx-4 mb-3">
+      <div class="col-lg-3 col-md-6">
+        <TripCard />
+      </div>
+      <div class="col-lg-3 col-md-6">
+        <TripCard />
+      </div>
+      <div class="col-lg-3 col-md-6">
+        <TripCard />
+      </div>
+      <div class="col-lg-3 col-md-6">
+        <TripCard />
+      </div>
+    </div>
+  </div>
+</template>
 
 <script>
+import TripCard from "@/components/common/TripCard.vue";
+
+
 export default {
   name: "MyTrips",
-  data() {},
+  data() { },
+  components: {
+    TripCard
+  },
   methods: {
-    getUser() {},
   },
 };
 </script>
 
-<style></style>
+<style>
+
+</style>
