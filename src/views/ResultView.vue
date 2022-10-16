b9e389dbc408ca9ef4197c4e3489eec41059abf8
ngkangting <benji.ngkt@gmail.com>
2022-10-16 22:17:29 +0800

    Changes

--- src/views/ResultView.vue
+++ src/views/ResultView.vue
@@ -1,53 +1,66 @@
 <template>
   <div class="container-fluid">
-    <h3 class="text-center fw-semibold mb-4">Result</h3>
+    <h4 class="text-center fw-semibold mb-4">Result</h4>
     <div class="row">
       <div class="col col-lg-8">
         <div class="card w-100 border-0">
           <div class="card-body">
-            <div class="mb-4">
-              <div class="row mb-4">
-                <div class="col-8">
-                  <img
-                    class="w-100 rounded"
-                    src="../assets/img/mbs-1.jpeg"
-                    alt="..."
-                    height="350"
-                  />
-                </div>
-                <div class="col-4 d-flex flex-column justify-content-between">
-                  <div class="row">
-                    <div class="col-12">
-                      <img
-                        class="w-100 rounded"
-                        src="../assets/img/mbs-2.jpg"
-                        alt="..."
-                        height="165"
-                      />
-                    </div>
+            <div class="row mb-3">
+              <div class="col-8">
+                <img
+                  class="w-100 rounded"
+                  src="../assets/img/mbs-1.jpeg"
+                  alt="..."
+                  height="350"
+                />
+              </div>
+              <div class="col-4 d-flex flex-column justify-content-between">
+                <div class="row">
+                  <div class="col-12">
+                    <img
+                      class="w-100 rounded"
+                      src="../assets/img/mbs-2.jpg"
+                      alt="..."
+                      height="165"
+                    />
                   </div>
-                  <div class="row">
-                    <div class="col-12">
-                      <img
-                        class="w-100 rounded"
-                        src="../assets/img/mbs-3.jpeg"
-                        alt="..."
-                        height="165"
-                      />
-                    </div>
+                </div>
+                <div class="row">
+                  <div class="col-12">
+                    <img
+                      class="w-100 rounded"
+                      src="../assets/img/mbs-3.jpeg"
+                      alt="..."
+                      height="165"
+                    />
                   </div>
                 </div>
               </div>
             </div>
-            <h5 class="card-title">Marina Bay Sands</h5>
-            <p class="card-text">
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
+            <div class="row">
+              <div class="col-5">
+                <div class="bg-light p-2 h-100 rounded">Test</div>
+              </div>
+              <div class="col-7">
+                <h5 class="card-title">Marina Bay Sands</h5>
+                <p class="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+                <p class="card-text">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Quisque id diam vel quam elementum pulvinar etiam non quam.
+                  Elementum eu facilisis sed odio. Nibh sit amet commodo nulla.
+                  Dui nunc mattis enim ut tellus elementum sagittis vitae.
+                </p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
+
+      <!--  -->
       <div class="col col-lg-4">Test</div>
     </div>
   </div>
