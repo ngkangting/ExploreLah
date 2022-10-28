826653d1a62b9452f5cf2f98a9cc2620141b90c2
desireely <105425207+desireely@users.noreply.github.com>
2022-10-28 15:30:53 +0800

    created discover page

--- src/views/AboutUs.vue
+++ src/views/AboutUs.vue
@@ -1,33 +1,19 @@
 <template>
   <div>
-    <div
-      class="row d-flex align-items-center justify-content-center mb-5"
-      style="background-color: #d6f4ff"
-      :style="{ height: 'calc(100vh - 75px)' }"
-    >
+    <div class="row d-flex align-items-center justify-content-center mb-5" style="background-color: #d6f4ff"
+      :style="{ height: 'calc(100vh - 75px)' }">
       <div class="col-lg-6">
         <h1 class="display-4 fw-bold lh-1 text-center ps-5">
           We help you make exploring Singapore easier than ever.
         </h1>
       </div>
       <div class="col-lg-6 d-flex justify-content-center pe-5">
-        <lottie-player
-          src="https://assets3.lottiefiles.com/private_files/lf30_5i5tlydx.json"
-          background="transparent"
-          speed="1"
-          style="width: 400px; height: 400px"
-          hover
-          loop
-          autoplay
-        ></lottie-player>
+        <lottie-player src="https://assets3.lottiefiles.com/private_files/lf30_5i5tlydx.json" background="transparent"
+          speed="1" style="width: 400px; height: 400px" hover loop autoplay></lottie-player>
       </div>
     </div>
-    <div
-      v-motion
-      :initial="{ opacity: 0, y: 100 }"
-      :enter="{ opacity: 1, y: 0 }"
-      class="row d-flex align-items-center justify-content-center p-5 my-5 mx-5"
-    >
+    <div v-motion :initial="{ opacity: 0, y: 100 }" :enter="{ opacity: 1, y: 0 }"
+      class="row d-flex align-items-center justify-content-center p-5 my-5 mx-5">
       <h3 style="color: #ff8397; font-weight: bold">About Us</h3>
       <p class="lead">
         ExploreLah! enables you to quickly and easily explore Singapore’s
@@ -44,25 +30,12 @@
     </div>
   </div>
 
-  <div
-    v-motion-slide-visible-once-left
-    class="row row-cols-1 row-cols-md-2 mx-5 mb-5"
-  >
+  <div v-motion-slide-visible-once-left class="row row-cols-1 row-cols-md-2 mx-5 mb-5">
     <div class="col">
-      <div
-        class="card h-100 text-center p-5"
-        style="background-color: #174877; border: none; color: white"
-      >
+      <div class="card h-100 text-center p-5" style="background-color: #174877; border: none; color: white">
         <div class="mx-auto">
-          <lottie-player
-            src="https://assets1.lottiefiles.com/packages/lf20_x9h8ar8l.json"
-            background="transparent"
-            speed="1"
-            style="width: 200px; height: 200px"
-            hover
-            loop
-            autoplay
-          >
+          <lottie-player src="https://assets1.lottiefiles.com/packages/lf20_x9h8ar8l.json" background="transparent"
+            speed="1" style="width: 200px; height: 200px" hover loop autoplay>
           </lottie-player>
         </div>
         <div class="card-body">
@@ -78,10 +51,7 @@
       </div>
     </div>
     <div class="col">
-      <div
-        class="card h-100 text-center p-5"
-        style="border-color: #174877; border: solid 2px; color: #174877"
-      >
+      <div class="card h-100 text-center p-5" style="border-color: #174877; border: solid 2px; color: #174877">
         <div class="card-body">
           <h3 class="card-title">Our Vision</h3>
           <p class="card-text">
@@ -91,15 +61,8 @@
           </p>
         </div>
         <div class="mx-auto">
-          <lottie-player
-            src="https://assets6.lottiefiles.com/packages/lf20_swnrn2oy.json"
-            background="transparent"
-            speed="1"
-            style="width: 300px; height: 300px"
-            hover
-            loop
-            autoplay
-          >
+          <lottie-player src="https://assets6.lottiefiles.com/packages/lf20_swnrn2oy.json" background="transparent"
+            speed="1" style="width: 300px; height: 300px" hover loop autoplay>
           </lottie-player>
         </div>
       </div>
@@ -111,10 +74,12 @@
 export default {
   name: "AboutUs",
   components: {},
-  data() {},
+  data() { },
 
   methods: {},
 };
 </script>
 
-<style></style>
+<style>
+
+</style>
