8f9a12814f27f5c7d70260ac4f57a9f32738af0f
desireely <105425207+desireely@users.noreply.github.com>
2022-10-29 22:11:41 +0800

    fixed about us card

--- src/components/common/AboutCard.vue
+++ src/components/common/AboutCard.vue
@@ -2,7 +2,10 @@
   <div class="col">
     <div class="card w-100" style="border:none">
       <div class="card-body">
-        <i :class="icon" class="rounded-circle" style="font-size:30px; background-color:#d6f4ff; padding:10px"></i>
+        <div class="d-flex justify-content-center align-items-center rounded-circle"
+          style="height: 60px; width: 60px; background-color: #d6f4ff">
+          <i :class="icon" style="font-size: 1.6rem"></i>
+        </div>
         <h3 class="card-title mt-3">{{ title }}</h3>
         <h5 class="card-text mt-3 text-muted">{{ subtitle }}</h5>
         <p class="card-text">{{ content }}</p>
