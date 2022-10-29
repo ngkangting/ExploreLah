2942deb5fb5190a54192153694a51166636c5ea7
desireely <105425207+desireely@users.noreply.github.com>
2022-10-29 16:46:33 +0800

    content update

---
+++ src/components/common/AboutCard.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="col">
+    <div class="card w-100" style="border:none">
+      <div class="card-body">
+        <i :class="icon" class="rounded-circle p-3" style="font-size:30px; background-color:#d6f4ff;"></i>
+        <h5 class="card-title mt-3">{{ title }}</h5>
+        <p class="card-text">{{ content }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AboutCard",
+  props: {
+    icon: String,
+    title: String,
+    content: String
+  },
+  data() {
+    return {};
+  },
+  methods: {}
+};
+</script>
+
+<style>
+
+</style>
--- src/views/AboutUs.vue
+++ src/views/AboutUs.vue
@@ -1,25 +1,23 @@
 <template>
-  <div>
-    <div class="row d-flex align-items-center justify-content-center mb-5" style="background-color: #d6f4ff"
-      :style="{ height: 'calc(100vh - 75px)' }">
-      <div class="col-lg-6">
-        <h1 class="display-3 fw-bold lh-1 text-center p-5">
-          We help you make exploring Singapore easier than ever.
-        </h1>
-        <h3 class="lh-1 text-center px-5 text-muted w-75 mx-auto">The
-          all-in-one platform to create your travel plans.
-        </h3>
-      </div>
-      <div class="col-lg-6 d-flex justify-content-center align-items-center p-5">
-        <lottie-player class="w-75 mx-auto" src="https://assets3.lottiefiles.com/private_files/lf30_5i5tlydx.json"
-          background="transparent" speed="1" loop autoplay></lottie-player>
-      </div>
+  <div class="row d-flex align-items-center justify-content-center mb-5" style="background-color: #d6f4ff"
+    :style="{ height: 'calc(100vh - 75px)' }">
+    <div class="col-lg-6">
+      <h1 class="display-3 fw-bold lh-1 text-center p-5">
+        We help you make exploring Singapore easier than ever.
+      </h1>
+      <h3 class="lh-1 text-center px-5 text-muted w-75 mx-auto">The
+        all-in-one platform to create your travel plans.
+      </h3>
+    </div>
+    <div class="col-lg-6 d-flex justify-content-center align-items-center p-5">
+      <lottie-player class="w-75 mx-auto" src="https://assets3.lottiefiles.com/private_files/lf30_5i5tlydx.json"
+        background="transparent" speed="1" loop autoplay></lottie-player>
     </div>
   </div>
 
   <div v-motion-slide-visible-once-left class="px-5 text-center mb-5">
-    <h3 style="color: #ff8397; font-weight: bold">About Us</h3>
-    <h5 style="color:#174877">Creating a community of explorers</h5>
+    <h1 style="color: #ff8397; font-weight: bold">About Us</h1>
+    <h3 style="color:#174877">Creating a community of explorers</h3>
     <p class="lead w-50 mx-auto pt-3">
       ExploreLah! enables you to quickly and easily explore Singapore’s offerings and create personalized itineraries by
       utilizing local expertise and cutting-edge artificial intelligence.
@@ -39,7 +37,7 @@
           </lottie-player>
         </div>
         <div class="card-body">
-          <h3 class="card-title" style="color: #ff8397; font-weight: bold">Our Mission</h3>
+          <h2 class="card-title" style="color: #ff8397; font-weight: bold">Our Mission</h2>
           <p class="card-text lead">
             To create a platform that makes planning trips quick and efficient,
             enabling more travelers to explore the wonders of Singapore and solve their various
@@ -52,7 +50,7 @@
       <div class="card h-100 text-center p-5 my-auto mx-auto"
         style="border-color: #174877; border: solid 2px; color: #174877">
         <div class="card-body">
-          <h3 class="card-title" style="color: #ff8397; font-weight: bold">Our Vision</h3>
+          <h2 class="card-title" style="color: #ff8397; font-weight: bold">Our Vision</h2>
           <p class="card-text lead">
             By making planning trips around Singapore easier, we hope to
             effectively promote inbound tourism for Singapore so more people are able
@@ -69,13 +67,36 @@
     </div>
   </div>
 
+  <div v-motion-slide-visible-once-left class="p-5">
+    <h1 class="text-center mb-5" style="color: #ff8397; font-weight: bold">Learn more about Us</h1>
+    <div class="row mx-auto align-items-center">
+      <AboutCard v-for="(card, index) in cards" :key="index" :icon="card.icon" :title="card.title"
+        :content="card.content">
+      </AboutCard>
+    </div>
+  </div>
+
 </template>
 
 <script>
+import AboutCard from "@/components/common/AboutCard.vue";
+
 export default {
   name: "AboutUs",
-  components: {},
-  data() { },
+
+  components: {
+    AboutCard
+  },
+
+  data() {
+    return {
+      cards: [
+        { icon: "bi bi-people", title: "Our Team", content: "hi" },
+        { icon: "bi bi-book", title: "Our Story", content: "bye" },
+        { icon: "bi bi-code-slash", title: "How we do it", content: "hi" }
+      ]
+    }
+  },
 
   methods: {},
 };
