0345477acaa27707dd2c5c293b89602233aadaff
ngkangting <benji.ngkt@gmail.com>
2022-11-13 02:29:46 +0800

    Nice

--- src/components/common/MerlionMascot.vue
+++ src/components/common/MerlionMascot.vue
@@ -1,131 +1,153 @@
 <template>
-    <div class="mask" @mouseover="hover = true" @mouseleave="hover = false" :style="merlionPos">
-        <img class="merlionHand"   src="../../assets/img/merlion/merlionHand.png" ref="merlion"  />
-        <img v-if="!hover" class="merlionBody"   src="../../assets/img/merlion/merlionBaseWEye.png" ref="merlion"  />
-        <img v-else class="merlionBody" src="../../assets/img/merlion/merlionBaseWink.png" ref="merlion"   />
-    </div>
-
+  <div
+    class="mask"
+    @mouseover="hover = true"
+    @mouseleave="hover = false"
+    :style="merlionPos"
+  >
+    <img
+      class="merlionHand"
+      :style="merlionHand"
+      src="../../assets/img/merlion/merlionHand.png"
+      ref="merlion"
+    />
+    <img
+      v-if="!hover"
+      class="merlionBody"
+      :style="merlionBodyStyle"
+      src="../../assets/img/merlion/merlionBaseWEye.png"
+      ref="merlion"
+    />
+    <img
+      v-else
+      class="merlionBody"
+      :style="merlionBodyStyle"
+      src="../../assets/img/merlion/merlionBaseWink.png"
+      ref="merlion"
+    />
+    Mouse position is at: {{ x }}, {{ y }}
+  </div>
 </template>
-  
+
 <script>
 import { useMouseDistanceFromElement } from "vue-composable";
-
-
+import { useMouse } from "../../helpers/mouse.js";
 
 export default {
-name: "MerlionMascot",
-props: {
+  name: "MerlionMascot",
+  props: {
     posX: String,
     posY: String,
-},
-data() {
+    isMovable: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  data() {
     return {
-        mouseX : null,
-        mouseY : null,
-        anchorX: 0,
-        anchorY: 0,
-        eyeRotation: null,
-        hover:false,
-        
-
-
+      mouseX: null,
+      mouseY: null,
+      anchorX: 0,
+      anchorY: 0,
+      eyeRotation: null,
+      hover: false,
     };
-},
-methods : {
+  },
+  setup() {
+    const { x, y } = useMouse();
+    return { x, y };
+  },
+  methods: {
     calculateDistance(mouseX, mouseY) {
-        //element.getBoundingClientRect().left
-        return Math.floor(Math.sqrt(Math.pow(mouseX - this.left, 2) + Math.pow(mouseY - this.top, 2)));      
-        },
+      //element.getBoundingClientRect().left
+      return Math.floor(
+        Math.sqrt(
+          Math.pow(mouseX - this.left, 2) + Math.pow(mouseY - this.top, 2)
+        )
+      );
+    },
     mouseMove(event) {
+      // this.getAngle(event.clientX, event.clientY, this.anchorX, this.anchorY)
+      this.mouseX = event.clientX;
+      this.mouseY = event.clientY;
 
-        // this.getAngle(event.clientX, event.clientY, this.anchorX, this.anchorY)
-        this.mouseX = event.clientX;
-        this.mouseY = event.clientY;
-        // let {distance} = useMouseDistanceFromElement(this.$refs.merlion)
+      let { distance } = useMouseDistanceFromElement(this.$refs.merlion);
 
-        // console.log(distance.value)
-        // console.log(this.$refs.merlion);
-        // console.log(this.mouseX, this.mouseY);
-        // console.log(element)
+      // console.log(distance.value)
+      // console.log(this.$refs.merlion);
+      // console.log(this.mouseX, this.mouseY);
+      // console.log(element)
 
-        // console.log(this.calculateDistance(this.mouseX, this.mouseY))
-        
+      console.log(this.calculateDistance(this.x, this.y));
     },
+  },
+  computed: {
+    merlionBodyStyle() {
+      let top = 85;
+      let left = -12;
+      let halfScreenY = window.screen.height / 1.5;
+      let halfScreenX = window.screen.width / 1.5;
+      let difference = 0;
 
+      if (this.y > halfScreenY) {
+        difference = this.y - halfScreenY;
+      }
 
-},
-computed : {        
-    leftEyeStyle(){
-        return `transform:rotate(${90 + this.eyeRotation}deg);top: -198px;left: 107px`
-    },
-    rightEyeStyle(){
-        return `transform:rotate(${90 + this.eyeRotation}deg);top: 463px;left: 183px;`
-    },
-    merlionStyle(){
-        return `position:absolute;bottom:${this.positionY}px;left:${this.positionX}`
-    },
-    left(){
-        return this.$refs.merlion.getBoundingClientRect().x;
-    },
-    top(){
-        return this.$refs.merlion.getBoundingClientRect().y;
+      difference = Math.min(85, difference);
 
+      console.log(difference);
+
+      return `position: absolute; top: ${top}px; left: ${left}px; animation: merlionDuck 0.5s ease 2 alternate;`;
     },
-    mouseDistance(){
-        
-        return this.distance;
+    merlionHand() {
+      let top = 85;
+      let left = -15;
+      let halfScreenY = window.screen.height / 1.5;
+      let halfScreenX = window.screen.width / 1.5;
+      let difference = 0;
+
+      if (this.y > halfScreenY) {
+        difference = this.y - halfScreenY;
+      }
+
+      return `position:absolute; z-index: 1; top:${top}px; left: ${left}px; `;
     },
-    merlionPos(){
-        console.log(`top:${this.posY}px;left:${this.posX}px`);
-        return `bottom:${this.posY}px;left:${this.posX}px`
-    }
-
-    
-
-},
-mounted(){  
-    console.log(this.posX, this.posY)
-
-}
-}
+    left() {
+      return this.$refs.merlion.getBoundingClientRect().x;
+    },
+    top() {
+      return this.$refs.merlion.getBoundingClientRect().y;
+    },
+    mouseDistance() {
+      return this.distance;
+    },
+    merlionPos() {
+      console.log(`top:${this.posY}px;left:${this.posX}px`);
+      return `bottom:${this.posY}px;left:${this.posX}px`;
+    },
+  },
+  mounted() {
+    console.log(this.posX, this.posY);
+  },
+};
 </script>
 
 <style lang="scss">
-
-
-
-.mask{
-    // background-color: red;
-    height:250px;
-    width:250px;
-    position: fixed;
-    overflow: hidden;
-    scale: .5;
+.mask {
+  // background-color: red;
+  height: 250px;
+  width: 250px;
+  position: fixed;
+  overflow: hidden;
+  scale: 0.5;
 }
 
-.merlionBody{
-    animation: merlionDuck 0.5s ease 2 alternate;
-    position: absolute;
-    top:85px;
-    left: -25px;
+@keyframes merlionDuck {
+  0% {
+    top: 85px;
+  }
+  100% {
+    top: 95px;
+  }
 }
-
-.merlionHand{
-    position: absolute;
-    z-index: 1;
-    top:85px;
-    left:-25px;
-}
-
-
-@keyframes merlionDuck{
-    0%{
-        top: 85px;
-    }
-    100%{
-        top:95px;
-    }
-}
-
-
 </style>
