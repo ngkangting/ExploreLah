35fab77d57a1e5c2d6a593cb209c0e343d10bf01
Xun Yi <xunyi.work@gmail.com>
2022-11-12 11:49:51 +0800

    Merlion with hover

--- src/components/common/MerlionMascot.vue
+++ src/components/common/MerlionMascot.vue
@@ -1,92 +1,99 @@
 <template>
-    <div class="mask" ref="merlion">
-        <img id ="anchor"  src="../../assets/img/merlion/merlionBaseWEye.png"  />
+    
+    <div class="mask" @mouseover="hover = true" @mouseleave="hover = false" >
+        <img class="merlionHand"   src="../../assets/img/merlion/merlionHand.png" ref="merlion"  />
+        <img v-if="!hover" class="merlionBody"   src="../../assets/img/merlion/merlionBaseWEye.png" ref="merlion"  />
+        <img v-else class="merlionBody" src="../../assets/img/merlion/merlionBaseWink.png" ref="merlion"   />
     </div>
-
-    <!-- <div id="eyes" @mouseenter="mouseEnter" @mousemove="mousemove">
-        <img class="mascot" src="../../assets/img/merlion/merlionEye.png" :style='leftEyeStyle'/>
-        <img class="mascot" src="../../assets/img/merlion/merlionEye.png" :style='rightEyeStyle'/> 
-    </div> -->
-    <!-- <img class="mascot" src="../../assets/img/merlion/merlionHand.png"/> -->
 
 </template>
   
 <script>
 import { useMouseDistanceFromElement } from "vue-composable";
 
-  export default {
-    name: "MerlionMascot",
-    props: {
-        positionX: String,
-        positionY: String,
-    },
-    data() {
-      return {
-          mouseX : null,
-          mouseY : null,
-          anchorX: 0,
-          anchorY: 0,
-          eyeRotation: null,
-  
-      };
-    },
-    methods : {
-        calculateDistance(mouseX, mouseY) {
-            //element.getBoundingClientRect().left
-            return Math.floor(Math.sqrt(Math.pow(mouseX - this.left, 2) + Math.pow(mouseY - this.top, 2)));      
-         },
-        mouseMove(event) {
 
-            // this.getAngle(event.clientX, event.clientY, this.anchorX, this.anchorY)
-            this.mouseX = event.clientX;
-            this.mouseY = event.clientY;
-            // console.log(this.$refs.merlion);
-            // console.log(this.mouseX, this.mouseY);
-            // console.log(element)
 
-            // console.log(this.calculateDistance(this.mouseX, this.mouseY))
-            
-        },
-
-    },
-    computed : {
-        leftEyeStyle(){
-            return `transform:rotate(${90 + this.eyeRotation}deg);top: -198px;left: 107px`
-        },
-        rightEyeStyle(){
-            return `transform:rotate(${90 + this.eyeRotation}deg);top: 463px;left: 183px;`
-        },
-        merlionStyle(){
-            return `position:absolute;bottom:${this.positionY}px;left:${this.positionX}`
-        },
-        left(){
-            return this.$refs.merlion.getBoundingClientRect().x;
-        },
-        top(){
-            return this.$refs.merlion.getBoundingClientRect().y;
-
-        }
-
+export default {
+name: "MerlionMascot",
+props: {
+    positionX: String,
+    positionY: String,
+},
+data() {
+    return {
+        mouseX : null,
+        mouseY : null,
+        anchorX: 0,
+        anchorY: 0,
+        eyeRotation: null,
+        hover:false,
         
 
-    },
-    mounted(){  
-        
-        window.addEventListener('mousemove', this.mouseMove);
-        const anchor = document.getElementById("anchor");
-        const rekt = anchor.getBoundingClientRect();
-        const anchorX = rekt.left + rekt.width / 2;
-        const anchorY = rekt.top + rekt.height / 2;
 
-        //Trying get X and Y of merlion...
-        console.log("Please fucking work")
-        console.log(this.$refs.merlion)
-        console.log(this.$refs.merlion.getBoundingClientRect().left);
-        console.log(this.$refs.merlion.getBoundingClientRect().top);
+    };
+},
+methods : {
+    calculateDistance(mouseX, mouseY) {
+        //element.getBoundingClientRect().left
+        return Math.floor(Math.sqrt(Math.pow(mouseX - this.left, 2) + Math.pow(mouseY - this.top, 2)));      
+        },
+    mouseMove(event) {
+
+        // this.getAngle(event.clientX, event.clientY, this.anchorX, this.anchorY)
+        this.mouseX = event.clientX;
+        this.mouseY = event.clientY;
+        // let {distance} = useMouseDistanceFromElement(this.$refs.merlion)
+
+        // console.log(distance.value)
+        // console.log(this.$refs.merlion);
+        // console.log(this.mouseX, this.mouseY);
+        // console.log(element)
+
+        // console.log(this.calculateDistance(this.mouseX, this.mouseY))
+        
+    },
+    hoverMerlion(){
+        console.log("Fuck u");
+        this.hover = !this.hover
     }
-  };
+
+},
+computed : {
+    leftEyeStyle(){
+        return `transform:rotate(${90 + this.eyeRotation}deg);top: -198px;left: 107px`
+    },
+    rightEyeStyle(){
+        return `transform:rotate(${90 + this.eyeRotation}deg);top: 463px;left: 183px;`
+    },
+    merlionStyle(){
+        return `position:absolute;bottom:${this.positionY}px;left:${this.positionX}`
+    },
+    left(){
+        return this.$refs.merlion.getBoundingClientRect().x;
+    },
+    top(){
+        return this.$refs.merlion.getBoundingClientRect().y;
+
+    },
+    mouseDistance(){
+        
+        return this.distance;
+    }
+
+    
+
+},
+mounted(){  
+    const {distance} = useMouseDistanceFromElement();
+    console.log(`Distance here is ${distance.value}`)
+    const {distance2} = useMouseDistanceFromElement(this.$refs.merlion);
+    console.log(`Distance here is ${distance.value}`)
+    console.log(this.$refs.merlion)
+
+}
+}
 </script>
-  
+
 <style lang="scss">
 
 
@@ -96,10 +103,32 @@ import { useMouseDistanceFromElement } from "vue-composable";
     height:250px;
     width:250px;
     position: fixed;
+    overflow: hidden;
+}
 
+.merlionBody{
+    animation: merlionDuck 0.5s ease 2 alternate;
+    position: absolute;
+    top:85px;
+    left: -25px;
+}
+
+.merlionHand{
+    position: absolute;
+    z-index: 1;
+    top:85px;
+    left:-25px;
 }
 
 
+@keyframes merlionDuck{
+    0%{
+        top: 85px;
+    }
+    100%{
+        top:95px;
+    }
+}
+
 
 </style>
-  
